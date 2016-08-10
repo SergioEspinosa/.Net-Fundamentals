51fe208bc0018ecbd78373fb515d23b245bb450e
Alex Aldazabal <berczeck69@gmail.com>
2016-08-10 00:04:26 -0500

    SOLID code

---
+++ Design Fundamentals Part I/Src/DesignFundamentalsPartI/SOLID/1. Single Responsability/Before/Code.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.IO;
+using System.Net.Mail;
+using System.Text;
+using System.Transactions;
+
+namespace SOLID._1._Single_Responsability.Before
+{
+    class Poliza { }
+    class PolizaHandler
+    {
+        public void Handle(Poliza poliza)
+        {
+            try
+            {
+                #region Trace
+                File.AppendAllText(@"c:\temp\log.txt", $"{nameof(PolizaHandler)} started at {DateTime.Now}");
+                #endregion
+
+                #region Process
+
+                using (var scope = new TransactionScope())
+                {
+                    var servicioPoliza = new ServicioWebPolizaExterno();
+                    servicioPoliza.Opera105874(new RequestOpera105874());
+                }
+                //Code
+                #endregion
+
+                #region Notification
+                MailMessage mail = new MailMessage("you@yourcompany.com", "user@hotmail.com");
+                SmtpClient client = new SmtpClient();
+                client.Port = 25;
+                client.Host = "smtp.google.com";
+                mail.Subject = "this is a test email.";
+                mail.Body = "this is my test email body";
+                client.Send(mail);
+                #endregion
+
+                #region Trace
+                File.AppendAllText(@"c:\temp\log.txt", $"{nameof(PolizaHandler)} finished at {DateTime.Now}");
+                #endregion
+            }
+            catch (Exception excepcion)
+            {
+                int contExcepcionInterna = 0;
+                var builder = new StringBuilder();
+                builder.Append("Exception.Type" +  excepcion.GetType().Name);
+                builder.Append("Exception.Message" + excepcion.Message);
+
+                Exception excepcionInterna = excepcion.InnerException;
+                while (excepcionInterna != null)
+                {
+                    contExcepcionInterna++;
+
+                    string tituloExcepcionInterna = string.Format("Exception.InnerException{0}", contExcepcionInterna);
+
+                    builder.Append(tituloExcepcionInterna + ".Type" + excepcion.InnerException.GetType().Name);
+                    builder.Append(tituloExcepcionInterna + ".Message" +  excepcion.InnerException.Message);
+
+                    excepcionInterna = excepcionInterna.InnerException;
+                }
+
+                if (excepcion.StackTrace != null)
+                {
+                    builder.Append("Exception.StackTrace" + excepcion.StackTrace);
+                }
+                File.AppendAllText(@"c:\temp\error.txt", $"{builder} occurred at {DateTime.Now}");
+            }
+           
+        }
+    }
+
+    internal class RequestOpera105874
+    {
+    }
+
+    internal class ServicioWebPolizaExterno
+    {
+        public void Opera105874(RequestOpera105874 requestCaducarPoliza)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Design Fundamentals Part I/Src/DesignFundamentalsPartI/SOLID/2. Open Closed/Before/Code.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace SOLID._2._Open_Closed.Before
+{
+    class ServicioEncriptacion
+    {
+        public string Encriptar(TipoEncriptacion tipo, string texto)
+        {
+            switch (tipo)
+            {
+                case TipoEncriptacion.Aes:
+                    using (Aes aesAlg = Aes.Create())
+                    {
+                        // Create a decrytor to perform the stream transform.
+                        ICryptoTransform encryptor = aesAlg.CreateEncryptor(aesAlg.Key, aesAlg.IV);
+
+                        // Create the streams used for encryption.
+                        using (MemoryStream msEncrypt = new MemoryStream())
+                        {
+                            using (
+                                CryptoStream csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write))
+                            {
+                                using (StreamWriter swEncrypt = new StreamWriter(csEncrypt))
+                                {
+                                    //Write all data to the stream.
+                                    swEncrypt.Write(texto);
+                                }
+                                return System.Text.Encoding.Default.GetString(msEncrypt.ToArray()); 
+                            }
+                        }
+                    }
+                case TipoEncriptacion.Sha256:
+                    StringBuilder Sb = new StringBuilder();
+
+                    using (SHA256 hash = SHA256Managed.Create())
+                    {
+                        Encoding enc = Encoding.UTF8;
+                        Byte[] result = hash.ComputeHash(enc.GetBytes(texto));
+
+                        foreach (Byte b in result)
+                            Sb.Append(b.ToString("x2"));
+                    }
+
+                    return Sb.ToString();
+                case TipoEncriptacion.Md5:
+                    using (MD5 md5Hash = MD5.Create())
+                    {
+                        byte[] data = md5Hash.ComputeHash(Encoding.UTF8.GetBytes(texto));
+
+                        // Create a new Stringbuilder to collect the bytes
+                        // and create a string.
+                        StringBuilder sBuilder = new StringBuilder();
+
+                        // Loop through each byte of the hashed data 
+                        // and format each one as a hexadecimal string.
+                        for (int i = 0; i < data.Length; i++)
+                        {
+                            sBuilder.Append(data[i].ToString("x2"));
+                        }
+
+                        // Return the hexadecimal string.
+                        return sBuilder.ToString();
+                    }
+                default:
+                    throw new Exception("Not supported");
+            }
+        }
+    }
+
+    public enum TipoEncriptacion
+    {
+        Aes,
+        Md5,
+        Sha256
+    }
+}
