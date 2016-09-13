703fbff77dc2fb8da6036398be13c52b66109484
Alex Aldazabal <berczeck69@gmail.com>
2016-09-12 23:57:21 -0500

    Car Rental service added

---
+++ WCF Car Rental Service/CarRentalService/Class1.cs
@@ -0,0 +1,65 @@
+﻿namespace CarRentalService
+{
+    using System.ServiceModel;
+    using System.Runtime.Serialization;
+
+    [DataContract]
+    public class ReservationInfo
+    {
+        [DataMember]
+        public int VehicleClass;
+        [DataMember]
+        public int Location;
+        [DataMember]
+        public string Dates;
+        [DataMember]
+        public string CorrelationId;
+    }
+
+    [ServiceContract]
+    interface IReservations
+    {
+        [OperationContract]
+        bool Check(ReservationInfo info);
+        [OperationContract(IsOneWay =true)]
+        void Reserve(ReservationInfo info);
+        [OperationContract]
+        bool Cancel(int confirmationNumber);
+        [OperationContract]
+        int GetStats();
+        [OperationContract]
+        int GetConfirmationNumber(string correlationId);
+    }
+
+    public class RentalReservations : IReservations
+    {
+        public bool Check(ReservationInfo info)
+        {
+            bool availability = true;
+            // logic to check availability goes here
+            return availability;
+        }
+        
+        public void Reserve(ReservationInfo info)
+        {
+            // logic to reserve rental car goes here
+            System.Threading.Thread.Sleep(10000);
+        }
+
+        public bool Cancel(int confirmationNumber)
+        {
+            bool success = false;
+            // logic to cancel reservation goes here
+            return success;
+        }
+
+        public int GetStats()
+        {
+            int numberOfReservations = 0;
+            // logic to determine reservation count goes here
+            return numberOfReservations;
+        }
+
+        public int GetConfirmationNumber(string correlationId) => correlationId.Length + 58457;
+    }
+}
---
+++ WCF Car Rental Service/Client/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var serviceHttp = new HttpCarRentalService.ReservationsClient();
+            serviceHttp.ClientCredentials.UserName.UserName = "alex";
+            serviceHttp.ClientCredentials.UserName.Password = "123456";
+            var checkHttp = serviceHttp.Check(null);
+
+            var serviceTcp = new TcpCarRenatlService.ReservationsClient();
+            var checkTcp = serviceTcp.Check(null);
+
+            Console.WriteLine($"Check HTTP: {checkHttp}");
+            Console.WriteLine($"Check TPC: {checkHttp}");
+
+            Console.ReadLine();
+        }
+    }
+}
